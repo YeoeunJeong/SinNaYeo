5334a4785bf43a76cd070f80d9c4ec9d3f676ce0
Jeong Yeo-Eun <jye930217@gmail.com>
2015-09-01 13:10:01 +0900

    android: 액티비티 역할 나누는중 #1

--- sny_android/SinNaYeoAndroid/src/swc/sny/activity/SinNaYeoMainActivity.java
+++ sny_android/SinNaYeoAndroid/src/swc/sny/activity/SinNaYeoMainActivity.java
@@ -18,23 +18,5 @@ public class SinNaYeoMainActivity extends Activity {
 		.setTitle("???????????? ???????????????").setMessage("IP??? ???????????????")
 		.setPositiveButton("??????", null).show();
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.sin_na_yeo_main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+ 
 }
