8ec8e4b8f61ef67fd697a74e5904dff779101645
Jeong Yeo-Eun <jye930217@gmail.com>
2015-09-04 17:38:18 +0900

    android:내가지금 뭘하고있는건지 잘모르겠다아... :o

--- sny_android/SinNaYeoAndroid/src/swc/sny/activity/SettingActivity.java
+++ sny_android/SinNaYeoAndroid/src/swc/sny/activity/SettingActivity.java
@@ -1,34 +1,44 @@
 package swc.sny.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class SettingActivity extends Activity {
+
+	EditText edTemper, edHumid;
+	ImageButton btnSave;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_setting);
+
+		init();
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.setting, menu);
-		return true;
-	}
+	private void init() {
+		edTemper = (EditText) findViewById(R.id.setting_set_temper_edit);
+		edHumid = (EditText) findViewById(R.id.setting_set_humid_edit);
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
+		btnSave = (ImageButton) findViewById(R.id.setting_save_btn);
+		btnSave.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(SettingActivity.this, "save", 0).show();
+				
+				Intent i = new Intent();
+				i.putExtra("setT", edTemper.getText().toString());
+				i.putExtra("setH", edHumid.getText().toString());
+				setResult(RESULT_OK, i);
+				
+				finish();
+			}
+		});
 	}
 }
